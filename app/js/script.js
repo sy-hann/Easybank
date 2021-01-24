c42f145fef1fefe12f24fd12e9ed5ebd21ff8bc3
thecoder523 <75168745+thecoder523@users.noreply.github.com>
2021-01-24 22:25:59 +0900

    Animated Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,19 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function(){
     console.log('open hamburger');
-    if(btnHamburger.classList.contains('open')){
-        btnHamburger.classList.remove('open');
-    } else {
-        btnHamburger.classList.add('open');
+
+    if(header.classList.contains('open')){ //Close Hamburger Menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else { //Open Hamburger Menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
     
 })
